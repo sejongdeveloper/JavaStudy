dc456c6984f7b1968d5006f96cc88d446785fdb4
sejongdeveloper <sejonghumble@naver.com>
2020-02-04 09:10:39 +0900

    *화면(파일) 입출력 컬랙션 프레임웤=>Properties

--- JavaBasic01/src/ch11/PropertiesEx3.java
+++ JavaBasic01/src/ch11/PropertiesEx3.java
@@ -7,6 +7,7 @@ public class PropertiesEx3 {
 	public static void main(String[] args) {
 		Properties prop = new Properties();
 		String path = PropertiesEx3.class.getResource("").getPath();
+		String path2 = "C:\\ide\\eGovFrameDev-3.8.0-64bit\\GitHub\\JavaStudy\\JavaBasic01\\src\\ch11\\";
 		
 		prop.setProperty("timeout", "30");
 		prop.setProperty("language", "한글");
@@ -14,8 +15,8 @@ public class PropertiesEx3 {
 		prop.setProperty("capacity", "10");
 		
 		try {
-			prop.store(new FileOutputStream(path+"output.txt"), "Properties Example");
-			prop.storeToXML(new FileOutputStream(path+"output.xml"), "Properties Example");
+			prop.store(new FileOutputStream(path2+"output.txt"), "Properties Example");
+			prop.storeToXML(new FileOutputStream(path2+"output.xml"), "Properties Example");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
