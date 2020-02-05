4d8ae4a3584b12bbf1f6052c482b750fed1e7251
sejongdeveloper <sejonghumble@naver.com>
2020-02-05 22:51:59 +0900

    *애너테이션 @Override, @Deprecated

---
+++ JavaBasic01/src/ch12/AnnotationEx1.java
@@ -0,0 +1,14 @@
+package ch12;
+
+class Parent {
+	void parentMethod() { }
+}
+
+class Child extends Parent {
+//	@Override
+	void parentmethod() { } // 조상 메서드의 이름을 잘못 적었음.
+}
+
+public class AnnotationEx1 {
+
+}
