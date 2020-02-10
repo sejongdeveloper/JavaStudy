a9ff8d9af2c51246ac3ab12896970116e26b6a8a
sejongdeveloper <sejonghumble@naver.com>
2020-02-10 22:52:35 +0900

    *쓰레드 intterrupt

---
+++ JavaBasic01/src/ch13/ThreadEx10.java
@@ -0,0 +1,39 @@
+package ch13;
+
+public class ThreadEx10 implements Runnable {
+	static boolean autoSave = false;
+	
+	public static void main(String[] args) {
+		Thread t = new Thread(new ThreadEx10());
+		t.setDaemon(true);
+		t.start();
+		
+		for(int i=0; i <=10; i++) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {}
+			System.out.println(i);
+			
+			if(i==5)
+				autoSave = true;
+		}
+	}
+	
+	@Override
+	public void run() {
+		while(true) {
+			try {
+				Thread.sleep(3 * 1000); // 3초마다
+			} catch (InterruptedException e) {}
+			
+			// autoSave의 값이 true이면 autoSave()를 호출한다.
+			if(autoSave) {
+				autoSave();
+			}
+		}
+	}
+	
+	public void autoSave() {
+		System.out.println("작업파일이 자동저장되었습니다.");
+	}
+}
