9ea30e2e5ac869599b7d095251b53f6493dd82a4
sejongdeveloper <sejonghumble@naver.com>
2020-02-22 21:14:03 +0900

    *Serialization Example

---
+++ JavaBasic01/src/ch15/MyData.java
@@ -0,0 +1,7 @@
+package ch15;
+
+import java.io.Serializable;
+
+public class MyData implements Serializable {
+	int value1;
+}
---
+++ JavaBasic01/src/ch15/SerialEx1.java
@@ -0,0 +1,36 @@
+package ch15;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+public class SerialEx1 {
+	public static void main(String[] args) {
+		try {
+			String fileName = "UserInfo.ser";
+			FileOutputStream fos = new FileOutputStream(fileName);
+			BufferedOutputStream bos = new BufferedOutputStream(fos);
+			
+			ObjectOutputStream out = new ObjectOutputStream(bos);
+			
+			UserInfo2 u1 = new UserInfo2("JavaMan", "1234", 30);
+			UserInfo2 u2 = new UserInfo2("JavaWoman", "4321", 26);
+			
+			ArrayList<UserInfo2> list = new ArrayList<>();
+			list.add(u1);
+			list.add(u2);
+			
+			// 객체를 직렬화한다.
+			out.writeObject(u1);
+			out.writeObject(u2);
+			out.writeObject(list);
+			out.close();
+			System.out.println("직렬화가 잘 끝났습니다.");
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	} // main
+} // class
