78825784d9ef978facea7dbda6a72e2e793e7563
sejongdeveloper <sejonghumble@naver.com>
2020-02-25 23:22:55 +0900

    *리터럴, printf, Scanner

---
+++ JavaBasic02/src/ch2/ScannerEx.java
@@ -0,0 +1,20 @@
+package ch2;
+
+import java.util.Scanner;
+
+public class ScannerEx {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("두자리 정수를 하나 입력해주세요.>");
+		String input = scanner.nextLine();
+		System.out.println("입력내용 :"+input);
+		try {
+			int num = Integer.parseInt(input); // 입력받은 문자열을 숫자로 변환
+			System.out.printf("num=%d%n", num);
+		} catch (NumberFormatException e) {
+			System.out.println("input :"+input);
+		}
+		
+	}
+}
