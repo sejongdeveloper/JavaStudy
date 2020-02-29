571bc01e60f3bb64de1cac0c93b3825aeac4fa61
sejongdeveloper <sejonghumble@naver.com>
2020-02-29 23:38:29 +0900

    *연산자

---
+++ JavaBasic02/src/ch3/OperatorEx11.java
@@ -0,0 +1,12 @@
+package ch3;
+
+public class OperatorEx11 {
+	public static void main(String[] args) {
+		char a = 'a';
+		char d = 'd';
+		char zero = '0';
+		char two = '2';
+		
+		
+	}
+}
---
+++ JavaBasic02/src/ch3/OperatorEx6.java
@@ -0,0 +1,10 @@
+package ch3;
+
+public class OperatorEx6 {
+	public static void main(String[] args) {
+		byte a = 10;
+		byte b = 20;
+//		byte c = a + b;
+//		System.out.println(c);
+	}
+}
