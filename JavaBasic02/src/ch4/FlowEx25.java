22d6630d496839effd12e4c937c61eb0de7cd6de
sejongdeveloper <sejonghumble@naver.com>
2020-03-07 16:11:22 +0900

    *while, break, continue, 이름 붙은 반복문

---
+++ JavaBasic02/src/ch4/FlowEx25.java
@@ -0,0 +1,24 @@
+package ch4;
+
+import java.util.Scanner;
+
+public class FlowEx25 {
+	public static void main(String[] args) {
+		int num = 0, sum = 0;
+		System.out.print("숫자를 입력하세요.(예:12345)>");
+		
+		Scanner scanner = new Scanner(System.in); // 화면을 통해 입력받은 내용을 tmp에 저장
+		String tmp = scanner.nextLine(); // 입력받은 문자열(tmp)을 숫자로 변환
+		num = Integer.parseInt(tmp);
+		
+		while(num!=0) {
+			// num을 10으로 나눈 나머지를 snm에 더함
+			sum += num%10; // sum = sum + sum%10;
+			System.out.printf("sum=%3d num=%d%n", sum, num);
+			
+			num /= 10; // num = num /10; num을 10으로 나눈 값을 다시 num에 저장
+		}
+		
+		System.out.println("각 자리수의 합:"+sum);
+	}
+}
