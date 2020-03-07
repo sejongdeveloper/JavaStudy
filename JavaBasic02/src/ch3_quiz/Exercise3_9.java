e4eee3984ab059767956200698d7725c8c0eabb2
sejongdeveloper <sejonghumble@naver.com>
2020-03-07 17:06:00 +0900

    *연산자 연습문제

---
+++ JavaBasic02/src/ch3_quiz/Exercise3_9.java
@@ -0,0 +1,9 @@
+package ch3_quiz;
+
+public class Exercise3_9 {
+	public static void main(String[] args) {
+		char ch = 'z';
+		boolean b = ('A'<=ch && ch <='Z') || ('a'<=ch && ch<='z') || ('0'<=ch && ch<='9');
+		System.out.println(b);
+	}
+}
