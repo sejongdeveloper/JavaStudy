8c111b44b6a6e0af105a7c9948b01e070e36b5c0
sejongdeveloper <sejonghumble@naver.com>
2020-03-18 09:09:35 +0900

    *오버로딩, 생성자

---
+++ JavaBasic02/src/ch06/ConstructorTest.java
@@ -0,0 +1,20 @@
+package ch06;
+
+class Data1 {
+	int value;
+}
+
+class Data2 {
+	int value;
+	
+	public Data2(int x) { // 매개변수가 있는 생성자.
+		value = x;
+	}
+}
+
+public class ConstructorTest {
+	public static void main(String[] args) {
+		Data1 d1 = new Data1();
+//		Data2 d2 = new Data2(); // compile error발생
+	}
+}
