1167a7807e77443b021ae819df1d0eceac22e542
sejongdeveloper <sejonghumble@naver.com>
2020-03-21 12:57:00 +0900

    *오버라이딩, super, 패키지

---
+++ JavaBasic02/src/ch07/PackageTest.java
@@ -0,0 +1,8 @@
+package ch07; // 빨간줄 없애기 위함
+//package com.javachobo.book; // 원래 코드
+
+public class PackageTest {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
